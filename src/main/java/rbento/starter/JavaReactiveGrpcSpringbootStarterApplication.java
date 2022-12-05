9e81ecac45ff08f0a815e6ce0dd39b8eeb0f0eb6
Rodrigo Bento <rodrigo.bento@gmail.com>
2022-12-05 18:41:25 -0500

    Update .gitignore

---
+++ src/main/java/rbento/starter/JavaReactiveGrpcSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package rbento.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactiveGrpcSpringbootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactiveGrpcSpringbootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rbento/starter/JavaReactiveGrpcSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package rbento.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactiveGrpcSpringbootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
